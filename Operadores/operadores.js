20632299f98b18274b8c28e21781f32ecf52ba44
claraaanabanana <134503855+claraaanabanana@users.noreply.github.com>
2023-08-22 09:14:44 -0300

    Add files via upload

---
+++ Operadores/operadores.js
@@ -0,0 +1,29 @@
+function somar(){
+    var s1 = document.getElementById('valor1').value;
+    var s2 = document.getElementById('valor2').value;
+    var s3 = parseInt(s1) + parseInt(s2);
+    alert (s3);
+    }
+    function subtrair(){
+    var s1 = document.getElementById('valor1').value;
+    var s2 = document.getElementById('valor2').value;
+    var s3 = parseInt(s1) - parseInt(s2);
+
+        alert(s3);
+    }
+    function mult(){
+        var s1 = document.getElementById('valor1').value;
+        var s2 = document.getElementById('valor2').value;
+        var s3 = parseInt(s1) * parseInt(s2);
+    
+            alert(s3);
+        }
+
+        
+        function div(){
+            var s1 = document.getElementById('valor1').value;
+            var s2 = document.getElementById('valor2').value;
+            var s3 = parseInt(s1) / parseInt(s2);
+        
+                alert(s3);
+            }
